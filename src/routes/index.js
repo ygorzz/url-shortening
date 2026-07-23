97e639500bfb337cc9b66f722ec432b644b1bc43
Ygor Santos <santosygor2007@gmail.com>
2026-07-22 23:48:53 -0300

    chore: update code design

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ import express from "express";
 import urlRoutes from "./urlRoutes.js"
 
 export default function routes (app) {
-    app.use(express.json(), urlRoutes);
+    app.use(express.json());
+    app.use(urlRoutes);
 }
